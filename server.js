149fe9d3ff396f92e37d73f04bfff388bae4ac04
manyu1225 <manyu1225@gmail.com>
2022-04-03 00:14:42 +0800

    add getTodo

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const http = require('http');
 const { v4: uuidv4 } = require('uuid');
 const errHandle = require('./errorHandle');
+const getTodo = require('./getTodo');
 const todos = [];
 
-const requestListener = (req, res)=>{
+const requestListener = (req, res) => {
     const headers = {
         'Access-Control-Allow-Headers': 'Content-Type, Authorization, Content-Length, X-Requested-With',
         'Access-Control-Allow-Origin': '*',
@@ -11,26 +12,27 @@ const requestListener = (req, res)=>{
         'Content-Type': 'application/json'
     }
     let body = "";
-    
-    req.on('data', chunk=>{
-        body+=chunk;
+
+    req.on('data', chunk => {
+        body += chunk;
     })
-    
-    if(req.url=="/todos" && req.method == "GET"){
+
+    if (req.url == "/todos" && req.method == "GET") {
         // getTodo.js
-    }else if(req.url=="/todos" && req.method == "POST"){
+        getTodo(res, headers, todos);
+    } else if (req.url == "/todos" && req.method == "POST") {
         // postTodo.js
-    }else if(req.url=="/todos" && req.method == "DELETE"){
+    } else if (req.url == "/todos" && req.method == "DELETE") {
         // deleteTodo.js
-    }else if(req.url.startsWith("/todos/") && req.method=="DELETE"){
+    } else if (req.url.startsWith("/todos/") && req.method == "DELETE") {
         // deleteTodo.js
-    }else if(req.url.startsWith("/todos/") && req.method=="PATCH"){
+    } else if (req.url.startsWith("/todos/") && req.method == "PATCH") {
         // patchTodo.js
-    }else if(req.method == "OPTIONS"){
-        res.writeHead(200,headers);
+    } else if (req.method == "OPTIONS") {
+        res.writeHead(200, headers);
         res.end();
-    }else{
-        res.writeHead(404,headers);
+    } else {
+        res.writeHead(404, headers);
         res.write(JSON.stringify({
             "status": "false",
             "message": "無此網站路由"
